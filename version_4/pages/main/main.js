fa171c31c2ac80b759506ac3d67859794489aa79
Ryan Graham <nashgryanb@gmail.com>
2015-12-04 11:35:15 -0700

    save

--- version_4/pages/main/main.js
+++ version_4/pages/main/main.js
@@ -1,8 +1,10 @@
 angular.module('timeClockApp')
-.controller('mainPageController', function($scope, $location, userService, fb, $firebaseAuth){
+.controller('mainPageController', function($scope, $location, userService, fb, $firebaseAuth, $firebaseObject, $firebaseArray){
 
     $scope.user = userService.getLoggedInUser();
 
+	var ref = new Firebase("https://time-clock-v1.firebaseio.com/");
+	$scope.data = $firebaseObject(ref);
 
     $scope.logout = function(){  
         userService.logout();
